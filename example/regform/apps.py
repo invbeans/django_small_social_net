9a83f927e8a9a981d97cc7e561189da057a5dbd5
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2021-09-05 18:16:18 +0400

    trying to move

--- example/regform/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegformConfig(AppConfig):
-    name = 'regform',
-    label = 'Форма для регистрации',
-    
--- example/regform/likecleaner.py
+++
@@ -1,8 +0,0 @@
-from .models import PostReact
-import datetime
-
-def clean_every_week():
-    like_recs = PostReact.objects.filter(react_type = 0)
-    for rec in like_recs:
-        if(datetime.timedelta(datetime.now() - rec.react_time).days > 30):
-            rec.delete()
