5eec0482f0e43c88a13c328c4aa0951822534c98
invbeans <74315761+invbeans@users.noreply.github.com>
2022-06-26 01:03:26 +0400

    .

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9)(temy&%99^lno=#lyxg_zmr@8!*bi#c^**k(k_8swdz+7__="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
